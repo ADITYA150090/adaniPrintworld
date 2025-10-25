6f3d79cf6ce3f7e31ef5db82de6f79baeb2d0282
Pranjal Jawalkar <pranjaljawalkar06@gmail.com>
2025-10-25 15:20:16 +0530

    setup directory structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import TEDashboard from "./pages/TE_Dashboard.jsx";
+import Home from "./pages/Home.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/tedashboard" element={<TEDashboard />} />
+
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/TE_Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TEDashboard = () => {
+  return (
+    <div>TEDashboard</div>
+  )
+}
+
+export default TEDashboard
